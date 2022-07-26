c4519480b152c5b953ae2339cf33f9fd658d2ac1
sankytech12 <sah.sasank12@gmail.com>
2022-07-26 08:59:42 +0530

    TicTacToe Game

---
+++ app/src/main/java/com/example/game/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    var Player=true
+    var Count=0
+    var boardStatus=Array(3){IntArray(3)}
+    lateinit var board:Array<Array<Button>>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        board= arrayOf(
+            arrayOf(btn1,btn2,btn3),
+            arrayOf(btn4,btn5,btn6),
+            arrayOf(btn7,btn8,btn9)
+        )
+        for(i in board){
+            for(button in i){
+                button.setOnClickListener(this)
+            }
+        }
+        initializeBoard()
+        resetId.setOnClickListener{
+            Player=true
+            Count=0
+            initializeBoard()
+        }
+    }
+
+    private fun initializeBoard() {
+        for(i in 0..2){
+            for(j in 0..2){
+                boardStatus[i][j]=-1
+                board[i][j].isEnabled=true
+                board[i][j].text=""
+            }
+        }
+    }
+
+    override fun onClick(view: View) {
+       when(view.id){
+           R.id.btn1 ->{
+                updateValue(row=0,col=0,player=Player)
+           }
+           R.id.btn2 ->{
+               updateValue(row=0,col=1,player=Player)
+           }
+           R.id.btn3 ->{
+               updateValue(row=0,col=2,player=Player)
+           }
+           R.id.btn4 ->{
+               updateValue(row=1,col=0,player=Player)
+           }
+           R.id.btn5 ->{
+               updateValue(row=1,col=1,player=Player)
+           }
+           R.id.btn6 ->{
+               updateValue(row=1,col=2,player=Player)
+           }
+           R.id.btn7 ->{
+               updateValue(row=2,col=0,player=Player)
+           }
+           R.id.btn8 ->{
+               updateValue(row=2,col=1,player=Player)
+           }
+           R.id.btn9 ->{
+               updateValue(row=2,col=2,player=Player)
+           }
+       }
+        Player=!Player
+        Count++
+        if(Player){
+            updateDisplay("Player X Turn")
+        }
+        else{
+            updateDisplay("Player O Turn")
+        }
+        if(Count==9){
+            updateDisplay("GAME DRAW")
+        }
+
+        checkWinner()
+    }
+
+    private fun checkWinner() {
+        //Horizontal CHeck
+        for(i in 0..2){
+            if(boardStatus[i][0]==boardStatus[i][1]&&boardStatus[i][0]==boardStatus[i][2]){
+                if(boardStatus[i][0]==1){
+                    updateDisplay("Player X Winner")
+                    break
+                }
+                else if(boardStatus[i][0]==0){
+                    updateDisplay("Player O Winner")
+                    break
+                }
+            }
+            if(boardStatus[0][i]==boardStatus[1][i]&&boardStatus[0][i]==boardStatus[2][i]){
+                if(boardStatus[0][i]==1){
+                    updateDisplay("Player X Winner")
+                    break
+                }
+                else if(boardStatus[0][i]==0){
+                    updateDisplay("Player O Winner")
+                    break
+                }
+            }
+        }
+        if(boardStatus[0][0]==boardStatus[1][1]&&boardStatus[0][0]==boardStatus[2][2]){
+            if(boardStatus[0][0]==1){
+                updateDisplay("Player X Winner")
+            }
+            else if(boardStatus[0][0]==0){
+                updateDisplay("Player O Winner")
+            }
+        }
+        if(boardStatus[0][2]==boardStatus[1][1]&&boardStatus[0][2]==boardStatus[2][0]){
+            if(boardStatus[0][2]==1){
+                updateDisplay("Player X Winner")
+            }
+            else if(boardStatus[0][2]==0){
+                updateDisplay("Player O Winner")
+            }
+        }
+    }
+
+    private fun updateDisplay(text: String) {
+        tvRes.text=text
+        if(text.contains("Winner")){
+            disableButton()
+        }
+    }
+
+    private fun disableButton() {
+        for(i in 0..2){
+            for(button in 0..2){
+                board[i][button].isEnabled=false
+            }
+        }
+    }
+
+    private fun updateValue(row: Int, col: Int, player: Boolean) {
+            val text:String=if(Player) "X" else "O"
+            val value:Int=if(Player) 1 else 0
+            board[row][col].apply {
+                isEnabled=false
+                setText(text)
+
+            }
+        boardStatus[row][col]=value
+    }
+}
